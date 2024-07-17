6c241fa1a8bed86622dedec9ce511cf186c24e61
Lourdes Genis <dermaeshop@gmail.com>
2024-07-17 12:38:25 -0600

    Commit de todos los cambios pendientes antes de cambiar de master a main

---
+++ Proyecto prueba/wp-content/uploads/uag-plugin/assets/1000/uag-js-1193.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-dadb4d59' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-27adbb15' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-9b532814' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-213f22d1' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-debbe8cd' );
+});
+ });
